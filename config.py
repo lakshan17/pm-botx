98bb0ce8e3360a94821af7a00f4eec8143d645d7
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-07-26 20:36:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 from os import getenv
 
 class Config(object):
-	APP_ID = int(os.environ.get("APP_ID"))
+	API_ID = int(os.environ.get("API_ID"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
